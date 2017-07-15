47a3caf2aae8a34d6324ff25ba26ae7399dbfb30
Marcos Garcia <1marcosgn@gmail.com>
2017-07-15 11:06:07 -0700

    - Adding basic structure for the project:
    * Slide Menu
    * Navigation Controller
    * Test Detail View Controller

---
+++ WhitePages/NavigationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationViewController.swift
+//  WhitePages
+//
+//  Created by marcosgn1 on 7/14/17.
+//  Copyright © 2017 Marcos Garcia. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
