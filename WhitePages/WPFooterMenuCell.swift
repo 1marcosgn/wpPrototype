9d9a0ea105fcf964de9c0158b5338321a7e08f78
Marcos Garcia <1marcosgn@gmail.com>
2017-07-15 22:56:06 -0700

    - Adding Menu View Controller Elements - Functionality not yet implemented

---
+++ WhitePages/WPFooterMenuCell.swift
@@ -0,0 +1,19 @@
+//
+//  WPFooterMenuCell.swift
+//  WhitePages
+//
+//  Created by marcosgn1 on 7/15/17.
+//  Copyright © 2017 Marcos Garcia. All rights reserved.
+//
+
+import UIKit
+
+class WPFooterMenuCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
